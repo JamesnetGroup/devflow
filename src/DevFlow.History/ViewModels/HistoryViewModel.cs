40c682dd5734d98710eccd0ec7dae7118cb8ada7
james.lee <james.lee@devncore.org>
2021-04-26 02:36:57 +0900

    Update buttons

---
+++ src/DevFlow.History/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DevFlow.Windowbase.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFlow.History.ViewModels
+{
+    public class HistoryViewModel : ObservableObject
+    {
+    }
+}
---
+++ src/DevFlow.History/Views/HistoryView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DevFlow.History.ViewModels;
+using System.Windows.Controls;
+
+namespace DevFlow.History.Views
+{
+    /// <summary>
+    /// HistoryView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class HistoryView : UserControl
+    {
+        public HistoryView()
+        {
+            InitializeComponent();
+            DataContext = new HistoryViewModel();
+        }
+    }
+}
