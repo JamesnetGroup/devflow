ec4db5729fc220dc91d15cf6b4c6f7abfbebc047
james <james.lee@devncore.org>
2021-05-07 11:19:35 +0900

    Update wallpaper

---
+++ src/DevFlow.Wallpaper/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using DevFlow.Controls;
+using DevFlow.Data;
+using DevFlow.History.Helper;
+using DevFlow.Windowbase.Mvvm;
+
+namespace DevFlow.History.ViewModels
+{
+    public class HistoryViewModel : ObservableObject
+    {
+        private List<HistoryModel> _histories;
+        private HistoryImageManager _imgLoader;
+        private HistoryModel _currentImage;
+
+        #region . Histories .
+
+        public List<HistoryModel> Histories
+        {
+            get { return _histories; }
+            set { _histories = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region . CurrentImage .
+
+        public HistoryModel CurrentImage
+        {
+            get { return _currentImage; }
+            set { _currentImage = value; OnPropertyChanged(); ImageSelected(value); }
+        }
+        #endregion
+
+        #region . Constructor .
+
+        public HistoryViewModel()
+        {
+            _imgLoader = HistoryImageManager.Instance;
+        }
+        #endregion
+
+        #region . OnLoaded . 
+
+        protected override void OnLoaded(UserControl view)
+        {
+            Histories = _imgLoader.GetHistories(Environment.CurrentDirectory);
+            _imgLoader.ThumbnailLoadAsync(Histories);
+
+            if (view is View v)
+            {
+                v.Minimizing = Minimizing;
+            }
+        }
+        #endregion
+
+        private void Minimizing(View obj)
+        {
+            if (obj.Parent is Grid grid)
+            {
+                grid.Children.Remove(obj);
+            }
+        }
+
+        #region . OnInitDesignTime .
+
+        protected override void OnInitDesignTime()
+        {
+            base.OnInitDesignTime();
+        }
+        #endregion
+
+        #region . ImageSelected . 
+
+        private void ImageSelected(HistoryModel value)
+        {
+            _imgLoader.PreviewLoadAsync(value);
+        }
+        #endregion
+    }
+}
---
+++ src/DevFlow.Wallpaper/Views/HistoryView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using DevFlow.Controls;
+using DevFlow.History.ViewModels;
+using System.Diagnostics;
+
+namespace DevFlow.History.Views
+{
+    public partial class HistoryView : View
+    {
+        #region . Constructor .
+
+        public HistoryView()
+        {
+            InitializeComponent();
+            DataContext = new HistoryViewModel();
+        }
+        #endregion
+    }
+}
