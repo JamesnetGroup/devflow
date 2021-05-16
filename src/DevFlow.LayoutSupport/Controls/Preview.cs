bcd9a876e045927196707ac96a72d5edb58b9522
james.lee <james.lee@devncore.org>
2021-05-16 13:34:43 +0900

    Update

--- src/DevFlow.LayoutSupport/Controls/Preview.cs
+++ src/DevFlow.LayoutSupport/Controls/Preview.cs
@@ -19,6 +19,22 @@ namespace DevFlow.LayoutSupport.Controls
 {
 	public class Preview : Widget
 	{
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(object), typeof(Preview), new PropertyMetadata(null));
+
+		public static readonly DependencyProperty SubTitleProperty = DependencyProperty.Register("SubTitle", typeof(object), typeof(Preview), new PropertyMetadata(null));
+
+		public object Title
+		{
+			get { return (object)this.GetValue(TitleProperty); }
+			set { this.SetValue(TitleProperty, value); }
+		}
+
+		public object SubTitle
+		{
+			get { return (object)this.GetValue(SubTitleProperty); }
+			set { this.SetValue(SubTitleProperty, value); }
+		}
+
 		static Preview()
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(Preview), new FrameworkPropertyMetadata(typeof(Preview)));
