dee8bb0120532b6c797e9dfb0da307a047bee9ec
kyj <elena.kim@devncore.org>
2021-05-27 10:50:18 +0900

    Update ColorSpoid

---
+++ src/DevFlow.LayoutSupport/Controls/ColorSpoid.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DevFlow.LayoutSupport.Controls
+{
+	public class ColorSpoid : Slider
+	{
+		static ColorSpoid()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorSpoid), new FrameworkPropertyMetadata(typeof(ColorSpoid)));
+		}
+	}
+}
