66399232e83ad70b401865768b57c29d712ad959
james <james.lee@devncore.org>
2021-06-01 12:00:28 +0900

    Update

---
+++ src/DevFlow.Colors/Local/Capture/CaptureProvider.cs
@@ -0,0 +1,84 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DevFlow.Colors.Local.Capture
+{
+	public class CaptureProvider
+    {
+        // 임시 버퍼의 graphic 타입
+        private Bitmap buffer = new Bitmap(1, 1);
+        private Graphics buffer_graphics = null;
+        private IKeyboardMouseEvents globalMouseHook;
+        public Action<byte[]> Extract = (p) => { };
+        public Action Exit = () => { };
+        public CaptureProvider()
+        {
+            this.buffer_graphics = Graphics.FromImage(buffer);
+        }
+
+        internal void Begin()
+        {
+            BeginCapture();
+        }
+
+        private void BeginCapture()
+        {
+            globalMouseHook = Hook.GlobalEvents();
+            globalMouseHook.MouseMove += MainWindow_MouseMove;
+            globalMouseHook.MouseDown += GlobalMouseHook_MouseDown;
+            Mouse.OverrideCursor = Cursors.Cross;
+        }
+
+
+		private void GlobalMouseHook_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            globalMouseHook.MouseUp += MainWindow_MouseUp;
+        }
+        private void EndCapture()
+        {
+            globalMouseHook.MouseMove -= MainWindow_MouseMove;
+            globalMouseHook.MouseUp -= MainWindow_MouseUp;
+            globalMouseHook.MouseDown -= GlobalMouseHook_MouseDown;
+            globalMouseHook.Dispose();
+            Mouse.OverrideCursor = null;
+        }
+        private void MainWindow_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            EndCapture();
+            Exit();
+        }
+        private void MainWindow_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            SetView(e.X, e.Y);
+        }
+
+        int count = 0;
+        private void SetView(int x, int y, bool capture = false)
+        {
+            if (count > 3)
+            {
+                count = 0;
+                var color = ScreenColor(x, y);
+
+                Extract(new byte[] { color.R, color.G, color.B, color.A });
+            }
+            else
+            {
+                count++;
+            }
+        }
+        private Color ScreenColor(int x, int y)
+        {
+            // Mouse 위치의 색을 추출한다.
+            this.buffer_graphics.CopyFromScreen(x, y, 0, 0, new System.Drawing.Size(1, 1));
+            // Pixel 값을 리턴한다.
+            return this.buffer.GetPixel(0, 0);
+        }
+	}
+}
--- src/DevFlow.Colors/ViewModels/ColorSpoidViewModel.cs
+++ src/DevFlow.Colors/ViewModels/ColorSpoidViewModel.cs
@@ -1,4 +1,5 @@
-﻿using DevFlow.Data.Colors;
+﻿using DevFlow.Colors.Local.Capture;
+using DevFlow.Data.Colors;
 using DevFlow.Windowbase.Mvvm;
 using System;
 using System.Collections.ObjectModel;
@@ -18,27 +19,40 @@ namespace DevFlow.Colors.ViewModels
         private string _currentColor;
         private string _reverseColor;
         private string _invertColor;
+		private bool _isCaptureColor;
+        
         private BitmapSource _captureImage;
-		private bool isCaptureColor;
 
-        #region Commands
+        private bool WaitCapture;
+        private CaptureProvider Capture;
 
-        public ICommand DragCaptureCommand { get; }
-        public ICommand ContentPasteCommand { get; }
-		#endregion
+		#region Commands
 
-		#region CaptureImage
+		public ICommand PasteCommand { get; }
+        public ICommand CaptureCommand { get; }
+        #endregion
 
-		public BitmapSource CaptureImage
+        #region CaptureImage
+
+        public BitmapSource CaptureImage
 		{
 			get { return _captureImage; }
 			set { _captureImage = value; OnPropertyChanged(); }
 		}
-		#endregion
+        #endregion
 
-		#region CurrentColor
+        #region IsCaptureColor
 
-		public string CurrentColor
+        public bool IsCaptureColor
+        {
+            get { return _isCaptureColor; }
+            set { _isCaptureColor = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region CurrentColor
+
+        public string CurrentColor
         {
             get { return _currentColor; }
             set { _currentColor = value; OnPropertyChanged(); }
@@ -68,7 +82,7 @@ namespace DevFlow.Colors.ViewModels
         public int Red
         {
 			get { return _red; }
-			set { _red = value; OnPropertyChanged(); SetRgb(); }
+			set { _red = value; OnPropertyChanged(); RgbChanged(); }
         }
 		#endregion
 
@@ -77,7 +91,7 @@ namespace DevFlow.Colors.ViewModels
 		public int Green
         {
             get { return _green; }
-            set { _green = value; OnPropertyChanged(); SetRgb(); }
+            set { _green = value; OnPropertyChanged(); RgbChanged(); }
         }
         #endregion
 
@@ -90,7 +104,7 @@ namespace DevFlow.Colors.ViewModels
 		public int Blue
 		{
 			get { return _blue; }
-			set { _blue = value; OnPropertyChanged(); SetRgb(); }
+			set { _blue = value; OnPropertyChanged(); RgbChanged(); }
 		}
 		#endregion
 
@@ -103,84 +117,100 @@ namespace DevFlow.Colors.ViewModels
 
 		public ColorSpoidViewModel()
         {
-            DragCaptureCommand = new RelayCommand<byte[]>(DragCapture);
-            ContentPasteCommand = new RelayCommand<object>(ContentPaste);
+            CaptureCommand = new RelayCommand<object>(BeginCapture);
+            PasteCommand = new RelayCommand<object>(Paste);
             ColorMap = new ObservableCollection<ColorStampModel>();
 
-            DragCapture(new byte[] { (byte)255, (byte)255, (byte)255, (byte)255 });
+            Capture = new CaptureProvider();
+            Capture.Extract = ShowColor;
+            Capture.Exit = () => IsCaptureColor = false;
+
+            ShowColor(new byte[] { (byte)255, (byte)255, (byte)255, (byte)255 });
         }
 		#endregion
 
-		private void SetRgb()
+		#region SetRgb
+
+		private void RgbChanged()
         {
-            if (!isCaptureColor)
+            if (!WaitCapture)
             {
-                DragCapture(new byte[] { (byte)Red, (byte)Green, (byte)Blue, (byte)Alpha });
+                ShowColor(new byte[] { (byte)Red, (byte)Green, (byte)Blue, (byte)Alpha });
             }
         }
+		#endregion
 
-        void ColorSelected(ColorStampModel color)
+		#region ColorSelected
+
+		void ColorSelected(ColorStampModel color)
         {
-            DragCapture(new byte[] { color.Red, color.Green, color.Blue, (byte)Alpha });
-        }
-
-        #region DragCapture
-
-        private void DragCapture(byte[] rgba)
-        {
-			//lock (lockObject)
-			{
-                isCaptureColor = true;
-
-                string r = rgba[0].ToString("X2");
-                string g = rgba[1].ToString("X2");
-                string b = rgba[2].ToString("X2");
-                string a = rgba[3].ToString("X2");
-                byte inv = 255;
-                string xr = ((byte)(inv - rgba[0])).ToString("X2");
-                string xg = ((byte)(inv - rgba[1])).ToString("X2");
-                string xb = ((byte)(inv - rgba[2])).ToString("X2");
-
-                CurrentColor = $"#{a}{r}{g}{b}";
-                InvertColor = $"#{a}{xr}{xg}{xb}";
-
-                if ((rgba[0] * 0.299 + rgba[1] * 0.587 + rgba[2] * 0.114) > 142)
-                {
-                    ReverseColor = "#FF000000";
-                }
-                else
-                {
-                    ReverseColor = "#FFFFFFFF";
-                }
-
-                Red = rgba[0];
-                Green = rgba[1];
-                Blue = rgba[2];
-
-                if (ColorMap.FirstOrDefault(x => x.HexColor == CurrentColor) is null)
-                {
-					ColorMap.Insert(0, new ColorStampModel
-					{
-						HexColor = CurrentColor,
-						Red = rgba[0],
-						Green = rgba[1],
-						Blue = rgba[2],
-						ColorClickCommand = new RelayCommand<ColorStampModel>(ColorSelected)
-                    });
-				}
-
-                if (ColorMap.Count == 65)
-                {   
-                    ColorMap.RemoveAt(ColorMap.Count - 1);
-                }
-                isCaptureColor = false;
-            }
+            ShowColor(new byte[] { color.Red, color.Green, color.Blue, (byte)Alpha });
         }
         #endregion
 
-        #region ContentPaste
+        #region BeginCapture
 
-        private void ContentPaste(object obj)
+        private void BeginCapture(object obj)
+        {
+            IsCaptureColor = true;
+            Capture.Begin();
+        }
+		#endregion
+
+		#region ShowColor
+
+		private void ShowColor(byte[] rgba)
+        {
+            WaitCapture = true;
+
+            string r = rgba[0].ToString("X2");
+            string g = rgba[1].ToString("X2");
+            string b = rgba[2].ToString("X2");
+            string a = rgba[3].ToString("X2");
+            byte inv = 255;
+            string xr = ((byte)(inv - rgba[0])).ToString("X2");
+            string xg = ((byte)(inv - rgba[1])).ToString("X2");
+            string xb = ((byte)(inv - rgba[2])).ToString("X2");
+
+            CurrentColor = $"#{a}{r}{g}{b}";
+            InvertColor = $"#{a}{xr}{xg}{xb}";
+
+            if ((rgba[0] * 0.299 + rgba[1] * 0.587 + rgba[2] * 0.114) > 142)
+            {
+                ReverseColor = "#FF000000";
+            }
+            else
+            {
+                ReverseColor = "#FFFFFFFF";
+            }
+
+            Red = rgba[0];
+            Green = rgba[1];
+            Blue = rgba[2];
+
+            if (ColorMap.FirstOrDefault(x => x.HexColor == CurrentColor) is null)
+            {
+                ColorMap.Insert(0, new ColorStampModel
+                {
+                    HexColor = CurrentColor,
+                    Red = rgba[0],
+                    Green = rgba[1],
+                    Blue = rgba[2],
+                    ColorClickCommand = new RelayCommand<ColorStampModel>(ColorSelected)
+                });
+            }
+
+            if (ColorMap.Count == 65)
+            {
+                ColorMap.RemoveAt(ColorMap.Count - 1);
+            }
+            WaitCapture = false;
+        }
+		#endregion
+
+		#region Paste
+
+		private void Paste(object obj)
         {
             if (obj is "COPY")
             {
