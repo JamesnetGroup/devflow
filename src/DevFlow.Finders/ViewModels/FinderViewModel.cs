2a576b7fde0e188b9e62ab3008e9d6f90580709d
james <james.lee@devncore.org>
2021-06-04 18:01:57 +0900

    Update

--- src/DevFlow.Finders/ViewModels/FinderViewModel.cs
+++ src/DevFlow.Finders/ViewModels/FinderViewModel.cs
@@ -1,8 +1,35 @@
-﻿using DevFlow.Windowbase.Mvvm;
+﻿using DevFlow.Finders.Local.Model;
+using DevFlow.Windowbase.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace DevFlow.Finders.ViewModels
 {
 	public class FinderViewModel : ObservableObject
 	{
+		private ObservableCollection<RootModel> _roots;
+		public ObservableCollection<RootModel> Roots
+		{
+			get { return _roots; }
+			set { _roots = value; OnPropertyChanged(); }
+		}
+
+		public FinderViewModel()
+		{
+			Roots = new ObservableCollection<RootModel>();
+			InitRoots();
+		}
+
+		private void InitRoots()
+		{
+			var myPC = new RootModel("MY PC");
+			myPC.Children.Add(new RootModel("Downloads"));
+			myPC.Children.Add(new RootModel("Documents"));
+			myPC.Children.Add(new RootModel("Pictures"));
+			myPC.Children.Add(new RootModel("Windows (C:)"));
+
+			Roots.Add(myPC);
+		}
 	}
 }
