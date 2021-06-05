b94c4b3cafaecdc99e5c9da879133eaca4ab1e47
james.lee <james.lee@devncore.org>
2021-06-05 23:42:50 +0900

    Revert "Update"
    This reverts commit 2a576b7fde0e188b9e62ab3008e9d6f90580709d.

--- src/DevFlow.Finders/ViewModels/FinderViewModel.cs
+++ src/DevFlow.Finders/ViewModels/FinderViewModel.cs
@@ -1,35 +1,8 @@
-﻿using DevFlow.Finders.Local.Model;
-using DevFlow.Windowbase.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using DevFlow.Windowbase.Mvvm;
 
 namespace DevFlow.Finders.ViewModels
 {
 	public class FinderViewModel : ObservableObject
 	{
-		private ObservableCollection<RootModel> _roots;
-		public ObservableCollection<RootModel> Roots
-		{
-			get { return _roots; }
-			set { _roots = value; OnPropertyChanged(); }
-		}
-
-		public FinderViewModel()
-		{
-			Roots = new ObservableCollection<RootModel>();
-			InitRoots();
-		}
-
-		private void InitRoots()
-		{
-			var myPC = new RootModel("MY PC");
-			myPC.Children.Add(new RootModel("Downloads"));
-			myPC.Children.Add(new RootModel("Documents"));
-			myPC.Children.Add(new RootModel("Pictures"));
-			myPC.Children.Add(new RootModel("Windows (C:)"));
-
-			Roots.Add(myPC);
-		}
 	}
 }
--- src/DevFlow.Finders/Views/Finder.cs
+++ src/DevFlow.Finders/Views/Finder.cs
@@ -13,6 +13,7 @@ namespace DevFlow.Finders.Views
 
 		public Finder()
 		{
+			DataContext = new FinderViewModel();
 		}
 	}
 }
